4a7f40a3580752aa26ea760c3f1ab80ceed4de1e
Sachindu Malshan <cst19035@std.uwu.ac.lk>
2025-02-17 18:13:43 +0000

    Solve prime number finding problem

--- src/Ch01-Arrays/01_05/begin/find-primes.cpp
+++ src/Ch01-Arrays/01_05/begin/find-primes.cpp
@@ -5,13 +5,27 @@ using namespace std;
 
 void findPrimes(const int arr[], size_t size, std::vector<int> &primes)
 {
-    // TODO: Implement
+    int temp_count = 0;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 1; j < arr[i]+1; j++)
+        {
+            if (arr[i] % j == 0)
+                temp_count++;
+        }
+
+        if (temp_count == 2)
+        {
+            primes.push_back(arr[i]);
+        }
+        temp_count = 0;
+    }
 }
 
 int main()
 {
-    int numbers[] {4, 7, 12, 3, 9, 17, 29};
-    
+    int numbers[]{4, 7, 12, 3, 9, 17, 29};
+
     size_t size = sizeof(numbers) / sizeof(numbers[0]);
     vector<int> primes;
 
