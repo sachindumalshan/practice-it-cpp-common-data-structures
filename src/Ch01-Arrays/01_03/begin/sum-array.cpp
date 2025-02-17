3fed9d6f54ab4d5ae74e4f84f2ff061586e60d85
Sachindu Malshan <cst19035@std.uwu.ac.lk>
2025-02-17 17:52:04 +0000

    Optimize the code

--- src/Ch01-Arrays/01_03/begin/sum-array.cpp
+++ src/Ch01-Arrays/01_03/begin/sum-array.cpp
@@ -3,13 +3,19 @@ using namespace std;
 
 int calculateSum(const int arr[], size_t size)
 {
-    int sum = 0;
-    for (int i = 0; i < size; i++)
+    if (size == 0)
+        return 0;
+    else if (size == 1)
+        return arr[0];
+    else
     {
-        sum = sum + arr[i];
+        int sum = 0;
+        for (int i = 0; i < size; i++)
+        {
+            sum = sum + arr[i];
+        }
+        return sum;
     }
-
-    return sum;
 }
 
 int main()
