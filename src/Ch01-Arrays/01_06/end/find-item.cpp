37e541838e2e4c24621f354c3132be53c8757549
Sachindu Malshan <cst19035@std.uwu.ac.lk>
2025-02-19 00:51:09 +0000

    Add missing type of passing arrays and set the return for else block

--- src/Ch01-Arrays/01_06/end/find-item.cpp
+++ src/Ch01-Arrays/01_06/end/find-item.cpp
@@ -7,24 +7,31 @@ int findItem(const T arr[], size_t size, T target)
 {
     for (int i = 0; i < size; i++)
     {
-        cout << arr[i] << endl;
-        if (arr[i] == target){
-            cout << "hi" << endl;
-            return 1;}
+        if (arr[i] == target)
+        {
+            return 1;
+        }
     }
+    return 0;
 }
 
 int main()
 {
     int a[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-    if (findItem(a, 10, 89))
+    if (findItem<int>(a, 10, 5))
         cout << "Target Found" << endl;
     else
         cout << "Target Not Found" << endl;
 
     char letter[8] = {'a', 'b', 'c', 'n', 'd', 'e', 'f', 'g'};
-    if (findItem(letter, 8, 'i'))
+    if (findItem<char>(letter, 8, 'e'))
+        cout << "Target Found" << endl;
+    else
+        cout << "Target Not Found" << endl;
+
+    double math[5] = {3.2,0.9,8.8,6,1.3};
+    if (findItem<double>(math, 5, 8.6))
         cout << "Target Found" << endl;
     else
         cout << "Target Not Found" << endl;
