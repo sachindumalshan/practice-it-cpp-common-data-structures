124d77324759a5c6fe9c3393f1a4242cf709e115
Sachindu Malshan <cst19035@std.uwu.ac.lk>
2025-02-21 18:33:23 +0000

    Initial commit for caller center problem

--- src/Ch03-Queues/03_03/end/call-center.cpp
+++ src/Ch03-Queues/03_03/end/call-center.cpp
@@ -1,52 +1,37 @@
 #include <iostream>
 #include <queue>
-using namespace std;
 
-class CallCenter
-{
-private:
-    queue<int> callQueue; // Queue to hold the call IDs
+class CallCenter{
 
-public:
-    // Function to enqueue a call
-    void receive_call(const int id)
-    {
-        callQueue.push(id);
-        cout << "Received call with ID: " << id << endl;
+    private:
+    std::queue<int> callerList;
+
+    public:
+    int process_call(){
+        while(!callerList.empty()){
+        std::cout << callerList.front() << std::endl;
+        callerList.pop();}
+
+        return -1;
+
     }
 
-    // Function to process a call
-    int process_call()
-    {
-        // Check if there are calls in the queue
-        if (callQueue.empty())
-        {            
-            cout << "No calls to process." << endl;
-            return -1; // Return -1 if no calls
-        }
-        else
-        {
-            int call_id = callQueue.front(); // Get the call id at the front of the queue
-            callQueue.pop();                 // Remove the call from the queue            
-            return call_id; // Return the call id
-        }
+    void receive_call(const int id){
+        callerList.push(id);
+        //std::cout << callerList.back() << std::endl;
     }
+
 };
 
 int main()
 {
-    CallCenter center; // Instantiate CallCenter object
+    CallCenter customer;
+    int callerID[] {1,2,3,4,5,6};
 
-    // Receiving some calls
-    center.receive_call(101);
-    center.receive_call(102);
-    center.receive_call(103);
+    for(int i : callerID)
+    customer.receive_call(i);
 
-    // Processing all calls
-    int call_id;
-    while((call_id = center.process_call()) != -1) {
-        std::cout << "Successfully processed call: " << call_id << std::endl;
-    }
-
+    customer.process_call();
+   
     return 0;
 }
