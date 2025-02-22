7945e06c8bd31cb5480d0ca8ae051b5f18e109c5
Sachindu Malshan <cst19035@std.uwu.ac.lk>
2025-02-22 04:51:28 +0000

    Display messages were created correctly

--- src/Ch03-Queues/03_03/end/call-center.cpp
+++ src/Ch03-Queues/03_03/end/call-center.cpp
@@ -1,37 +1,41 @@
 #include <iostream>
 #include <queue>
 
-class CallCenter{
+class CallCenter
+{
 
-    private:
+private:
     std::queue<int> callerList;
 
-    public:
-    int process_call(){
-        while(!callerList.empty()){
-        std::cout << callerList.front() << std::endl;
-        callerList.pop();}
+public:
+    int process_call()
+    {
+        while (!callerList.empty())
+        {
+            std::cout << "Caller '" << callerList.front() << "' is finished" << std::endl;
+            callerList.pop();
+        }
 
         return -1;
-
     }
 
-    void receive_call(const int id){
+    void receive_call(const int id)
+    {
         callerList.push(id);
-        //std::cout << callerList.back() << std::endl;
+        std::cout << "Caller '" << callerList.back() << "' added to the waiting list" << std::endl;
     }
-
 };
 
 int main()
 {
     CallCenter customer;
-    int callerID[] {1,2,3,4,5,6};
+    int callerID[]{1, 2, 3, 4, 5};
 
-    for(int i : callerID)
-    customer.receive_call(i);
+    for (int i : callerID)
+        customer.receive_call(i);
+    std::cout << "-----" << std::endl;
 
     customer.process_call();
-   
+
     return 0;
 }
