23943bf255b91addc713fd4c2b0e2138186af622
Sachindu Malshan <cst19035@std.uwu.ac.lk>
2025-02-23 07:03:59 +0000

    Code for caller center code implement

--- src/Ch03-Queues/03_03/end/call-center.cpp
+++ src/Ch03-Queues/03_03/end/call-center.cpp
@@ -1,3 +1,7 @@
+/*
+Author implement code base
+==========================
+
 #include <iostream>
 #include <queue>
 using namespace std;
@@ -47,6 +51,51 @@ int main()
     while((call_id = center.process_call()) != -1) {
         std::cout << "Successfully processed call: " << call_id << std::endl;
     }
+
+    return 0;
+}
+
+*/
+
+#include <iostream>
+#include <queue>
+
+class CallCenter
+{
+
+private:
+    std::queue<int> callerList;
+
+public:
+    int process_call()
+    {
+        while (!callerList.empty())
+        {
+            std::cout << "Caller '" << callerList.front() << "' is finished" << std::endl;
+            callerList.pop();
+        }
+
+        return -1;
+    }
+
+    void receive_call(const int id)
+    {
+        callerList.push(id);
+        std::cout << "Caller '" << callerList.back() << "' added to the waiting list" << std::endl;
+    }
+};
+
+int main()
+{
+    CallCenter customer;
+    int callerID[]{1, 2, 3, 4, 5};
+
+    for (int i : callerID)
+        customer.receive_call(i);
+    std::cout << "-----" << std::endl;
+
+    if (customer.process_call() == -1)
+        std::cout << "No calls to process" << std::endl;
 
     return 0;
 }
