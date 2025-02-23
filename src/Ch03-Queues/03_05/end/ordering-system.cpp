92292450df0ea2c4676d72f3ae54b53d4309f82e
Sachindu Malshan <cst19035@std.uwu.ac.lk>
2025-02-23 07:06:14 +0000

    Order processing system code implemented

--- src/Ch03-Queues/03_05/end/ordering-system.cpp
+++ src/Ch03-Queues/03_05/end/ordering-system.cpp
@@ -1,3 +1,7 @@
+/*
+Author implement code base
+==========================
+
 #include <iostream>
 #include <queue>
 using namespace std;
@@ -62,6 +66,74 @@ int main()
     while ((orderID = system.fulfill_order()) != -1)
     {
         cout << "Order with ID " << orderID << " has been fulfilled.\n";
+    }
+
+    return 0;
+}
+
+*/
+
+#include <iostream>
+#include <queue>
+
+class MerchOrderSystem
+{
+private:
+    std::queue<int> regularOrder;
+    std::queue<int> priorityOrder;
+
+public:
+    void place_order(int orderId, bool isPriority = false)
+    {
+        if (isPriority)
+        {
+            priorityOrder.push(orderId);
+            std::cout << "Priority order " << orderId << " added to the queue" << std::endl;
+        }
+
+        else
+        {
+            regularOrder.push(orderId);
+            std::cout << "Regular order " << orderId << " added to the queue" << std::endl;
+        }
+    }
+
+    int fullfill_order()
+    {
+        while (!priorityOrder.empty())
+        {
+            std::cout << "Priority order " << priorityOrder.front() << " - Processed" << std::endl;
+            priorityOrder.pop();
+        }
+
+        while (!regularOrder.empty())
+        {
+            std::cout << "Regular order " << regularOrder.front() << " - Processed" << std::endl;
+            regularOrder.pop();
+        }
+
+        return -1;
+    }
+};
+
+int main()
+{
+    MerchOrderSystem customer;
+
+    customer.place_order(1);
+    customer.place_order(2);
+    customer.place_order(3, true);
+    customer.place_order(4);
+    customer.place_order(5, true);
+    customer.place_order(6);
+    customer.place_order(7, true);
+    customer.place_order(8, true);
+
+    std::cout << "-----------------------" << std::endl;
+
+    if (customer.fullfill_order() == -1)
+    {
+        std::cout << "No orders to process" << std::endl;
     }
 
     return 0;
